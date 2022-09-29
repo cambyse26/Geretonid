e59585dbe9f6242ee96ea1faa2f5b2474fa7539b
cambyse26 <103960895+cambyse26@users.noreply.github.com>
2022-09-29 17:39:24 +0200

    Update and rename webflow_script12.js to webflow_script123.js

--- webflow_script123.js
+++ webflow_script123.js
@@ -148,16 +148,16 @@
             doc.text(orgaMail, 200, 50, null, null, "right");
             doc.setFont('Times-Roman', 'bold');
 
-            doc.text('Objet: Droit d\'accès', 10, 60, { maxWidth: 105 });
+            doc.text('Objet: Droit d\'accès', 10, 60, { maxWidth: 200 });
             doc.setFont('Times-Roman', 'normal')
-            doc.text('Madame, Monsieur,', 10, 70, { maxWidth: 105 });
-            doc.text('Je vous prie de bien vouloir m\'indiquer si des données me concernant figurent dans vos fichiers informatisés ou manuels.', 10, 80, { maxWidth: 105 });
-            doc.text('Dans l\'affirmative, je souhaiterais obtenir une copie, en langage clair, de l\'ensemble de ces et données (y compris celles figurant dans les zones « blocs-notes » ou « commentaires ») en application de l\'article 15 du Règlement général sur la protection des données (RGPD).', 10, 100, { maxWidth: 105 });
-            doc.text('Je vous remercie de me faire parvenir votre réponse dans les meilleurs délais et au plus tard dans un délai d\'un mois à compter de la réception de ma demande (article 12.3 du RGPD).', 10, 130, { maxWidth: 105 });
-            doc.text('A défaut de réponse de votre part dans les délais impartis ou en cas de réponse incomplète je me réserve la possibilité de saisir la Commission nationale de l\'informatique et des libertés (CNIL) d\'une réclamation.', 10, 155, { maxWidth: 105 });
-            doc.text('A toutes fins utiles, vous trouverez des informations sur le site internet de la CNIL : ', 10, 178, { maxWidth: 105 });
-            doc.text('https://www.cnil.fr/fr/professionnels-comment-repondre-une-demande-de-droit-dacces.', 10, 184,{ maxWidth: 105 });
-            doc.text('Je vous prie d\'agréer, Madame, Monsieur, l\'expression de mes salutations distinguées.', 10, 200, { maxWidth: 105 });
+            doc.text('Madame, Monsieur,', 10, 70, { maxWidth: 200 });
+            doc.text('Je vous prie de bien vouloir m\'indiquer si des données me concernant figurent dans vos fichiers informatisés ou manuels.', 10, 80, { maxWidth: 200 });
+            doc.text('Dans l\'affirmative, je souhaiterais obtenir une copie, en langage clair, de l\'ensemble de ces et données (y compris celles figurant dans les zones « blocs-notes » ou « commentaires ») en application de l\'article 15 du Règlement général sur la protection des données (RGPD).', 10, 100, { maxWidth: 200 });
+            doc.text('Je vous remercie de me faire parvenir votre réponse dans les meilleurs délais et au plus tard dans un délai d\'un mois à compter de la réception de ma demande (article 12.3 du RGPD).', 10, 130, { maxWidth: 200 });
+            doc.text('A défaut de réponse de votre part dans les délais impartis ou en cas de réponse incomplète je me réserve la possibilité de saisir la Commission nationale de l\'informatique et des libertés (CNIL) d\'une réclamation.', 10, 155, { maxWidth: 200 });
+            doc.text('A toutes fins utiles, vous trouverez des informations sur le site internet de la CNIL : ', 10, 178, { maxWidth: 200 });
+            doc.text('https://www.cnil.fr/fr/professionnels-comment-repondre-une-demande-de-droit-dacces.', 10, 184,{ maxWidth: 200 });
+            doc.text('Je vous prie d\'agréer, Madame, Monsieur, l\'expression de mes salutations distinguées.', 10, 200, { maxWidth: 200 });
 
             // PDF Footer
             doc.text(NP, 10, 240);
